8e38b770e9bbf7f9526dc77288d17a5c8340798a
Shvy-bit <jcusiq@unsa.edu.pe>
2026-05-28 23:05:15 -0500

    creacion de views

--- Lab07/ProjectPr/notas/views.py
+++ Lab07/ProjectPr/notas/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NotasForm
+from .models import NotasAlumnosPorCurso
 
-# Create your views here.
+def crear_venta(request):
+    if request.method == 'POST':
+        form = NotasForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_notas')
+    else:
+        form = NotasForm()
+    return render(request, 'notas/lista_notas.html', {'form': form})
+def lista_notas(request):
+    notas = NotasAlumnosPorCurso.objects.all()
+    return render(request, 'notas/lista_notas.html', {'notas': notas})
