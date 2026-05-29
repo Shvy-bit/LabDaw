c2b8dee1ae92a9458473b2d63507ab81148831ba
Shvy-bit <jcusiq@unsa.edu.pe>
2026-05-28 23:12:52 -0500

    arreglo de errores

--- Lab07/ProjectPr/notas/views.py
+++ Lab07/ProjectPr/notas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import NotasForm
 from .models import NotasAlumnosPorCurso
 
-def crear_venta(request):
+def guardar_notas(request):
     if request.method == 'POST':
         form = NotasForm(request.POST)
         if form.is_valid():
@@ -10,7 +10,7 @@ def crear_venta(request):
             return redirect('lista_notas')
     else:
         form = NotasForm()
-    return render(request, 'notas/crear_venta.html', {'form': form})
+    return render(request, 'notas/guardar_notas.html', {'form': form})
 def lista_notas(request):
     notas = NotasAlumnosPorCurso.objects.all()
     return render(request, 'notas/lista_notas.html', {'notas': notas})
